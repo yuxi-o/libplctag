d8bfabce4f8d8f7ec56ff3368ca91c2e9861e2b2
Kyle Hayes <kyle.hayes@gmail.com>
2017-10-29 11:38:43 -0700

    Refactored marshal/unmarshal for individual fields into macros.  Fixed bugs with send/receive packet.

--- src/protocols/ab/logix/packet.c
+++ src/protocols/ab/logix/packet.c
@@ -32,11 +32,34 @@
 
 
 /* byte order data */
-int byte_order_8[1] = {0};
+int byte_order_8[1]  = {0};
 int byte_order_16[2] = {0,1};
 int byte_order_32[4] = {0,1,2,3};
 int byte_order_64[8] = {0,1,2,3,4,5,6,7};
 
+
+
+#define MARSHAL_FIELD(BUF, BITS, FIELD)                                \
+    do {                                                               \
+        rc = bytebuf_set_int(BUF, (BITS)/8 , byte_order_ ##BITS , (int64_t)FIELD);\
+        if(rc != PLCTAG_STATUS_OK) {                                   \
+            pdebug(DEBUG_WARN,"Error inserting " #FIELD " field!");    \
+            return rc;                                                 \
+        }                                                              \
+    } while(0)
+
+
+#define UNMARSHAL_FIELD(BUF, BITS, TYPE, FIELD)                        \
+    do {                                                               \
+        rc = bytebuf_get_int(BUF, (BITS)/8 , byte_order_ ##BITS , &val);\
+        if(rc != PLCTAG_STATUS_OK) {                                   \
+            pdebug(DEBUG_WARN,"Error unmarshalling " #FIELD " field!");\
+            return rc;                                                 \
+        }                                                              \
+        if(FIELD) {                                                    \
+            *FIELD = (TYPE)(int ## BITS ## _t)val;                     \
+        }                                                              \
+    } while(0)
 
 
 
@@ -47,7 +70,15 @@ int marshal_eip_header(bytebuf_p buf,
                         uint64_t sender_context)  /* identifies the unconnected session packet */
 {
     int rc = PLCTAG_STATUS_OK;
+    int payload_size;
+    int status = 0;
+    int options = 0;
 
+    pdebug(DEBUG_DETAIL,"Starting.");
+
+    payload_size = bytebuf_get_size(buf);
+
+    /* make space at the beginning of the buffer. */
     rc = bytebuf_set_cursor(buf, -24); /* FIXME - MAGIC */
     if(rc != PLCTAG_STATUS_OK) {
         pdebug(DEBUG_WARN,"Error setting buffer cursor!");
@@ -55,56 +86,59 @@ int marshal_eip_header(bytebuf_p buf,
     }
 
     /* command */
-    bytebuf_set_int(buf, 2, byte_order_16, (int64_t)command);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting command field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 16, command);
+    //~ rc = bytebuf_set_int(buf, 2, byte_order_16, (int64_t)command);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting command field!");
+        //~ return rc;
+    //~ }
 
     /* payload length */
-    bytebuf_set_int(buf, 2, byte_order_16, (int64_t)bytebuf_size(buf));
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting payload size field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 16, payload_size);
+    //~ rc = bytebuf_set_int(buf, 2, byte_order_16, (int64_t)payload_size);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting payload size field!");
+        //~ return rc;
+    //~ }
 
     /* session handle */
-    bytebuf_set_int(buf, 4, byte_order_32, (int64_t)session_handle);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting session handle field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 32, session_handle);
+    //~ rc = bytebuf_set_int(buf, 4, byte_order_32, (int64_t)session_handle);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting session handle field!");
+        //~ return rc;
+    //~ }
 
     /* status, always zero on send */
-    bytebuf_set_int(buf, 2, byte_order_16, (int64_t)0);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting status field!");
-        return rc;
-    }
+    status = 0;
+    MARSHAL_FIELD(buf, 32, status);
+    //~ bytebuf_set_int(buf, 4, byte_order_32, (int64_t)0);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting status field!");
+        //~ return rc;
+    //~ }
 
     /* session packet identifier */
-    bytebuf_set_int(buf, 8, byte_order_64, (int64_t)sender_context);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting sender context field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 64, sender_context);
+    //~ rc = bytebuf_set_int(buf, 8, byte_order_64, (int64_t)sender_context);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting sender context field!");
+        //~ return rc;
+    //~ }
 
     /* options, zero always? */
-    bytebuf_set_int(buf, 4, byte_order_32, (int64_t)0);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting options field!");
-        return rc;
-    }
+    options = 0;
+    MARSHAL_FIELD(buf, 32, options);
+    //~ rc = bytebuf_set_int(buf, 4, byte_order_32, (int64_t)0);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting options field!");
+        //~ return rc;
+    //~ }
+
+    pdebug(DEBUG_DETAIL,"Done.");
 
     return PLCTAG_STATUS_OK;
 }
-
-    uint16_t encap_command;
-    uint16_t encap_length;
-    uint32_t encap_session_handle;
-    uint32_t encap_status;
-    uint64_t encap_sender_context;
-    uint32_t encap_options;
 
 
 int unmarshal_eip_header(bytebuf_p buf, uint16_t *command, uint16_t *length, uint32_t *session_handle, uint32_t *status, uint64_t *sender_context, uint32_t *options)
@@ -112,47 +146,57 @@ int unmarshal_eip_header(bytebuf_p buf, uint16_t *command, uint16_t *length, uin
     int rc = PLCTAG_STATUS_OK;
     int64_t val;
 
-    rc = bytebuf_get_int(buf, 2, byte_order_16, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting command field!");
-        return rc;
-    }
-    *command = (uint16_t)(int16_t)val;
+    pdebug(DEBUG_DETAIL,"Starting.");
 
-    rc = bytebuf_get_int(buf, 2, byte_order_16, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting length field!");
-        return rc;
-    }
-    *length = (uint16_t)(int16_t)val;
+    UNMARSHAL_FIELD(buf, 16, uint16_t, command);
+    //~ rc = bytebuf_get_int(buf, 2, byte_order_16, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting command field!");
+        //~ return rc;
+    //~ }
+    //~ *command = (uint16_t)(int16_t)val;
 
-    rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting session handle field!");
-        return rc;
-    }
-    *session_handle = (uint32_t)(int32_t)val;
+    UNMARSHAL_FIELD(buf, 16, uint16_t, length);
+    //~ rc = bytebuf_get_int(buf, 2, byte_order_16, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting length field!");
+        //~ return rc;
+    //~ }
+    //~ *length = (uint16_t)(int16_t)val;
 
-    rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting status field!");
-        return rc;
-    }
-    *status = (uint32_t)(int32_t)val;
+    UNMARSHAL_FIELD(buf, 32, uint32_t, session_handle);
+    //~ rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting session handle field!");
+        //~ return rc;
+    //~ }
+    //~ *session_handle = (uint32_t)(int32_t)val;
 
-    rc = bytebuf_get_int(buf, 8, byte_order_64, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting sender context field!");
-        return rc;
-    }
-    *sender_context = (uint64_t)val;
+    UNMARSHAL_FIELD(buf, 32, uint32_t, status);
+    //~ rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting status field!");
+        //~ return rc;
+    //~ }
+    //~ *status = (uint32_t)(int32_t)val;
 
-    rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error getting options field!");
-        return rc;
-    }
-    *options = (uint32_t)(int32_t)val;
+    UNMARSHAL_FIELD(buf, 64, uint64_t, sender_context);
+    //~ rc = bytebuf_get_int(buf, 8, byte_order_64, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting sender context field!");
+        //~ return rc;
+    //~ }
+    //~ *sender_context = (uint64_t)val;
+
+    UNMARSHAL_FIELD(buf, 32, uint32_t, options);
+    //~ rc = bytebuf_get_int(buf, 4, byte_order_32, &val);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error getting options field!");
+        //~ return rc;
+    //~ }
+    //~ *options = (uint32_t)(int32_t)val;
+
+    pdebug(DEBUG_DETAIL,"Done.");
 
     return rc;
 }
@@ -162,19 +206,25 @@ int marshal_register_session(bytebuf_p buf, uint16_t eip_version, uint16_t optio
 {
     int rc = PLCTAG_STATUS_OK;
 
+    pdebug(DEBUG_DETAIL,"Starting.");
+
     /* requested protocol version */
-    rc = bytebuf_set_int(buf, 2, byte_order_16, (int64_t)eip_version);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting eip_version field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 16, eip_version);
+    //~ rc = bytebuf_set_int(buf, 2, byte_order_16, (int64_t)eip_version);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting eip_version field!");
+        //~ return rc;
+    //~ }
 
     /* option flags, always zero? */
-    bytebuf_set_int(buf, 2, byte_order_16, (int64_t)option_flags);
-    if(rc != PLCTAG_STATUS_OK) {
-        pdebug(DEBUG_WARN,"Error inserting option_flags field!");
-        return rc;
-    }
+    MARSHAL_FIELD(buf, 16, option_flags);
+    //~ bytebuf_set_int(buf, 2, byte_order_16, (int64_t)option_flags);
+    //~ if(rc != PLCTAG_STATUS_OK) {
+        //~ pdebug(DEBUG_WARN,"Error inserting option_flags field!");
+        //~ return rc;
+    //~ }
+
+    pdebug(DEBUG_DETAIL,"Done.");
 
     return PLCTAG_STATUS_OK;
 }
@@ -188,6 +238,8 @@ int send_eip_packet(sock_p sock, uint16_t command, uint32_t session_handle, uint
     int rc = PLCTAG_STATUS_OK;
     int offset = 0;
 
+    pdebug(DEBUG_DETAIL,"Starting.");
+
     /* inject an EIP encapsulation header. */
     rc = marshal_eip_header(payload,command, session_handle, sender_context);
     if(rc != PLCTAG_STATUS_OK) {
@@ -199,22 +251,24 @@ int send_eip_packet(sock_p sock, uint16_t command, uint32_t session_handle, uint
     bytebuf_set_cursor(payload, 0);
 
     pdebug(DEBUG_DETAIL,"Sending packet:");
-    pdebug_dump_bytes(DEBUG_DETAIL,bytebuf_get_buffer(payload), bytebuf_size(payload));
+    pdebug_dump_bytes(DEBUG_DETAIL,bytebuf_get_buffer(payload), bytebuf_get_size(payload));
 
     /* send the data. */
     do {
-        rc = socket_write(sock, bytebuf_get_buffer(payload), bytebuf_size(payload) - offset);
+        rc = socket_write(sock, bytebuf_get_buffer(payload), bytebuf_get_size(payload) - offset);
         if(rc > 0) {
             offset += rc;
-            if(offset < bytebuf_size(payload)) {
+            if(offset < bytebuf_get_size(payload)) {
                 bytebuf_set_cursor(payload, offset);
             }
         }
-    } while(rc >= 0 && offset < bytebuf_size(payload));
+    } while(rc >= 0 && offset < bytebuf_get_size(payload));
 
     if(rc > 0) {
         rc = PLCTAG_STATUS_OK;
     }
+
+    pdebug(DEBUG_DETAIL,"Done.");
 
     return rc;
 }
@@ -226,6 +280,8 @@ int receive_eip_packet(sock_p sock, bytebuf_p buf)
     int rc = 0;
     int total_read = 0;
     int data_needed = EIP_ENCAP_SIZE; /* MAGIC */
+
+    pdebug(DEBUG_DETAIL,"Starting.");
 
     bytebuf_reset(buf);
 
@@ -249,6 +305,8 @@ int receive_eip_packet(sock_p sock, bytebuf_p buf)
         } else {
             total_read += rc;
 
+            pdebug(DEBUG_DETAIL,"Got %d more bytes of data.", rc);
+
             /* recalculate the amount of data needed if we have just completed the read of an encap header */
             if (total_read >= EIP_ENCAP_SIZE && data_needed == EIP_ENCAP_SIZE) {
                 uint16_t command;
@@ -257,6 +315,8 @@ int receive_eip_packet(sock_p sock, bytebuf_p buf)
                 uint32_t status;
                 uint64_t sender_context;
                 uint32_t options;
+
+                bytebuf_set_cursor(buf, 0);
 
                 /* parse the header. */
                 rc = unmarshal_eip_header(buf, &command, &length, &session_handle, &status, &sender_context, &options);
@@ -276,14 +336,16 @@ int receive_eip_packet(sock_p sock, bytebuf_p buf)
         }
     } while (rc > 0 && total_read < data_needed);
 
-    if(rc > 0) {
+    if(rc >= 0) {
         rc = PLCTAG_STATUS_OK;
 
         bytebuf_set_cursor(buf, 0);
 
         pdebug(DEBUG_DETAIL,"Received packet:");
-        pdebug_dump_bytes(DEBUG_DETAIL, bytebuf_get_buffer(buf), bytebuf_size(buf));
+        pdebug_dump_bytes(DEBUG_DETAIL, bytebuf_get_buffer(buf), bytebuf_get_size(buf));
     }
+
+    pdebug(DEBUG_DETAIL,"Done.");
 
     return rc;
 }
